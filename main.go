cc7fa97327e9f5e7403886158f40525e339e5f29
Enzo <enzo.carbonara@ynov.com>
2023-11-12 19:13:23 +0100

    commit test

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	temp, err := template.ParseGlob("./templates/*.html")
+	if err != nil {
+		fmt.Printf(fmt.Sprintf("ERREUR => %s", err.Error()))
+		return
+	}
+
+	http.HandleFunc("/debut", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "debut", nil)
+	})
+
+	rootDoc, _ := os.Getwd()
+	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
+	//Init serv
+	http.ListenAndServe("localhost:8080", nil)
+}
