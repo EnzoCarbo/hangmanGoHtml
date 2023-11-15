78017cfbaea0be940f5923529e8e48bc32508eb5
prplecloud <52612580+prplecloud@users.noreply.github.com>
2023-11-15 10:54:10 +0100

    routes added

--- main.go
+++ main.go
@@ -23,6 +23,18 @@ func main() {
 		temp.ExecuteTemplate(w, "debut", nil)
 	})
 
+	http.HandleFunc("/level1", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level1", nil)
+	})
+
+	http.HandleFunc("/level2", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level2", nil)
+	})
+
+	http.HandleFunc("/level3", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "level3", nil)
+	})
+
 	rootDoc, _ := os.Getwd()
 	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
