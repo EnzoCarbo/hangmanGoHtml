21421eca66b35a38a336725a03b77d6d4a773bc3
prplecloud <52612580+prplecloud@users.noreply.github.com>
2023-11-24 08:30:17 +0100

    get hangmaned

--- hangman/hangman.go
+++ hangman/hangman.go
@@ -1,17 +1,24 @@
 package hangman
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type Game struct {
-	State        string
-	Letters      []string
+	State        bool
+	Letters      []string // nul
 	FoundLetters []string
 	UsedLetters  []string
+	Word         string
 	TurnsLeft    int
 }
+
+var Player Game
 
 func New(turns int, word string) (*Game, error) {
 	if len(word) < 2 {
@@ -25,46 +32,20 @@ func New(turns int, word string) (*Game, error) {
 	}
 
 	g := &Game{
-		State:        "",
+		State:        true,
 		Letters:      letters,
 		FoundLetters: found,
 		UsedLetters:  []string{},
 		TurnsLeft:    turns,
+		Word:         word,
 	}
 
 	return g, nil
 }
 
-func (g *Game) MakeAGuess(guess string) {
-	guess = strings.ToUpper(guess)
-
-	switch g.State {
-	case "won", "lost":
-		return
-	}
-
-	if letterInWord(guess, g.UsedLetters) {
-		g.State = "alreadyGuessed"
-	} else if letterInWord(guess, g.Letters) {
-		g.State = "goodGuess"
-		g.RevealLetter(guess)
-
-		if hasWon(g.Letters, g.FoundLetters) {
-			g.State = "won"
-		}
-	} else {
-		g.State = "badGuess"
-		g.LoseTurn(guess)
-
-		if g.TurnsLeft <= 0 {
-			g.State = "lost"
-		}
-	}
-}
-
-func hasWon(letters []string, foundLetters []string) bool {
-	for i := range letters {
-		if letters[i] != foundLetters[i] {
+func HasWon(hiddenWord []string, word string) bool {
+	for i := range hiddenWord {
+		if string(hiddenWord[i]) != string(word[i]) {
 			return false
 		}
 	}
@@ -85,7 +66,7 @@ func (g *Game) LoseTurn(guess string) {
 	g.UsedLetters = append(g.UsedLetters, guess)
 }
 
-func letterInWord(guess string, letters []string) bool {
+func LetterInWord(guess string, letters []string) bool {
 	for _, l := range letters {
 		if l == guess {
 			return true
@@ -94,6 +75,87 @@ func letterInWord(guess string, letters []string) bool {
 	return false
 }
 
-func HangmanInit() {
-	New(10, "word")
+var words = make([]string, 0, 50)
+
+func Load(filename string) error { //charge le .txt avec les noms de champions
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func PickWord() string { //prend un nom de champion aléatoire
+	rand.Seed(time.Now().Unix())
+	i := rand.Intn(len(words))
+	return words[i]
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func ReadGuess() (guess string, err error) {
+	valid := false
+	for !valid {
+		fmt.Print("Quelle est votre lettre? ")
+		guess, err = reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+		guess = strings.TrimSpace(guess)
+
+		if len(guess) != 1 {
+			fmt.Println("Format invalide.", guess, len(guess))
+			continue
+		}
+		valid = true
+	}
+	return
+}
+
+func Start() {
+
+	err := Load("champions.txt")
+	if err != nil {
+		fmt.Printf("Could not load dictionary: %v\n", err)
+		os.Exit(1)
+	}
+
+	g, err := New(11, PickWord())
+	if err != nil {
+		fmt.Printf("Could not create game: %v\n", err)
+		os.Exit(1)
+	}
+	Player = *g
+}
+
+func (g *Game) CheckInput(value string) {
+
+	if len(value) != 1 {
+		if g.Word == value {
+		} else {
+			g.TurnsLeft = -2
+		}
+	} else {
+		IsFind := false
+		for i, v := range g.Word {
+			if value == string(v) {
+				IsFind = true
+				g.FoundLetters[i] = string(v)
+			}
+		}
+		if !(IsFind) {
+			g.TurnsLeft = -1
+		}
+
+	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"hangmanweb/hangman"
-	"html/template"
-	"net/http"
-	"os"
-)
+import "hangmanweb/hangman"
 
 type PageInit struct {
 	Username string
+	lvl      string
 }
 
 type Game struct {
@@ -24,56 +19,53 @@ var logs PageInit
 
 func main() {
 
-	temp, err := template.ParseGlob("./templates/*.html")
-	if err != nil {
-		fmt.Printf(fmt.Sprintf("ERREUR => %s", err.Error()))
-		return
-	}
+	hangman.Start()
+	hangman.Player.CheckInput("guess")
+	/*
+	   	temp, err := template.ParseGlob("./templates/*.html")
+	   	if err != nil {
+	   		fmt.Printf(fmt.Sprintf("ERREUR => %s", err.Error()))
+	   		return
+	   	}
 
-	http.HandleFunc("/intro", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "intro", nil)
-	})
+	   	http.HandleFunc("/intro", func(w http.ResponseWriter, r *http.Request) {
+	   		temp.ExecuteTemplate(w, "intro", nil)
+	   	})
 
-	http.HandleFunc("/whatsapp", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "whatsapp", nil)
-	})
+	   	http.HandleFunc("/whatsapp", func(w http.ResponseWriter, r *http.Request) {
+	   		temp.ExecuteTemplate(w, "whatsapp", nil)
+	   	})
 
-	http.HandleFunc("/username", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "username", nil)
+	   	http.HandleFunc("/init", func(w http.ResponseWriter, r *http.Request) {
+	   		temp.ExecuteTemplate(w, "username", nil)
 
-	})
+	   	})
 
-	http.HandleFunc("/treatment", func(w http.ResponseWriter, r *http.Request) {
-		logs = PageInit{
-			r.FormValue("pseudo")}
-		fmt.Println(logs)
+	   	http.HandleFunc("/init/treatment", func(w http.ResponseWriter, r *http.Request) {
+	   		logs = PageInit{r.FormValue("pseudo"), r.FormValue("lvl")}
+	   		fmt.Println(logs)
+	   		if logs.lvl == "1" {
+	   			hangman.Start("......")
+	   			http.Redirect(w, r, "/game", 301)
+	   		}
 
-		http.Redirect(w, r, "/debut", http.StatusMovedPermanently)
-		http.Redirect(w, r, "/level1", http.StatusMovedPermanently)
-		http.Redirect(w, r, "/level2", http.StatusMovedPermanently)
-		http.Redirect(w, r, "/level3", http.StatusMovedPermanently)
-	})
+	   	})
+	   	type PageGame struct {
+	   	}
+	   	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
 
-	http.HandleFunc("/debut", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "debut", logs)
-	})
+	   		temp.ExecuteTemplate(w, "easy", logs)
+	   	})
 
-	http.HandleFunc("/level1", func(w http.ResponseWriter, r *http.Request) {
-		hangman.HangmanInit()
-		temp.ExecuteTemplate(w, "easy", logs)
-	})
+	   	http.HandleFunc("/game/treatment", func(w http.ResponseWriter, r *http.Request) {
+	   	})
 
-	http.HandleFunc("/level2", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "medium", nil)
-	})
+	   	rootDoc, _ := os.Getwd()
+	   	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
+	   	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
+	   	//Init serv
+	   	http.ListenAndServe("localhost:8080", nil)
+	   }
+	*/
 
-	http.HandleFunc("/level3", func(w http.ResponseWriter, r *http.Request) {
-		temp.ExecuteTemplate(w, "hard", nil)
-	})
-
-	rootDoc, _ := os.Getwd()
-	fileserver := http.FileServer(http.Dir(rootDoc + "/asset"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
-	//Init serv
-	http.ListenAndServe("localhost:8080", nil)
 }
